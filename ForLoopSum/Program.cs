a1b533f61a297fd325e4e7d4283699696e4aed99
stensihver <sten.sihver@tktk.ee>
2024-09-24 11:09:11 +0300

    add Week4Loops code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+int sum = 0;
+
+for(int i = 3; i < 0; i++) 
+{
+   Console.WriteLine($"i = {i}");
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i;
+}
+
+
+Console.WriteLine($"Final total {sum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//FOR-LOOP
+
+
+int i = 1;
+for(i = 1; i < 4; i++)
+{
+    Console.WriteLine("We don't need no education");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
+
+//Console.WriteLine("We don't need no education");  
+ 
